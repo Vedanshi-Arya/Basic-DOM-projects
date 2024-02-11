b66a1c68959a23ef47acbf6e87bdb08b223c621c
Vedanshi-Arya <142818538+Vedanshi-Arya@users.noreply.github.com>
2024-02-11 14:41:54 +0530

    Add files via upload
    This project creates a post that we see on Instagram. When we double-click on the image, a white heart appears and after 1sec it disappears similarly to that on Instagram.

---
+++ like_post/like.js
@@ -0,0 +1,16 @@
+let a=document.querySelector('.container');
+let love=document.querySelector('i');
+
+
+a.addEventListener("dblclick",function(){
+    love.style.transform='translate(-50%,-50%) scale(1)';
+    love.style.opacity="0.9";
+    setTimeout(function(){
+       love.style.opacity=0;
+    },1000)
+    setTimeout(function(){
+        love.style.transform='translate(-50%,-50%) scale(0)';
+
+    },2000);
+    
+})
